3845f5781b26634fc8d2864cef1225a02b85eb76
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2021-01-05 23:21:48 +0530

    Added new files

---
+++ ermsdb.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 23, 2019 at 07:21 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ermsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empeducation`
+--
+
+CREATE TABLE `empeducation` (
+  `Id` int(11) NOT NULL,
+  `EmpID` int(10) DEFAULT NULL,
+  `CoursePG` varchar(45) DEFAULT NULL,
+  `SchoolCollegePG` varchar(45) DEFAULT NULL,
+  `YearPassingPG` varchar(45) DEFAULT NULL,
+  `PercentagePG` varchar(4) DEFAULT NULL,
+  `CourseGra` varchar(45) DEFAULT NULL,
+  `SchoolCollegeGra` varchar(45) DEFAULT NULL,
+  `YearPassingGra` varchar(45) DEFAULT NULL,
+  `PercentageGra` varchar(4) DEFAULT NULL,
+  `CourseSSC` varchar(45) DEFAULT NULL,
+  `SchoolCollegeSSC` varchar(45) DEFAULT NULL,
+  `YearPassingSSC` varchar(45) DEFAULT NULL,
+  `PercentageSSC` varchar(4) DEFAULT NULL,
+  `CourseHSC` varchar(45) DEFAULT NULL,
+  `SchoolCollegeHSC` varchar(45) DEFAULT NULL,
+  `YearPassingHSC` varchar(45) DEFAULT NULL,
+  `PercentageHSC` varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `empeducation`
+--
+
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empexpireince`
+--
+
+CREATE TABLE `empexpireince` (
+  `ID` int(11) NOT NULL,
+  `EmpID` varchar(5) DEFAULT NULL,
+  `Employer1Name` varchar(75) DEFAULT NULL,
+  `Employer1Designation` varchar(50) DEFAULT NULL,
+  `Employer1CTC` varchar(50) DEFAULT NULL,
+  `Employer1WorkDuration` varchar(11) DEFAULT NULL,
+  `Employer2Name` varchar(75) DEFAULT NULL,
+  `Employer2Designation` varchar(50) DEFAULT NULL,
+  `Employer2CTC` varchar(50) DEFAULT NULL,
+  `Employer2WorkDuration` varchar(11) DEFAULT NULL,
+  `Employer3Name` varchar(75) DEFAULT NULL,
+  `Employer3Designation` varchar(50) DEFAULT NULL,
+  `Employer3CTC` varchar(50) DEFAULT NULL,
+  `Employer3WorkDuration` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `empexpireince`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeedetail`
+--
+
+CREATE TABLE `employeedetail` (
+  `ID` int(11) NOT NULL,
+  `EmpFname` varchar(50) DEFAULT NULL,
+  `EmpLName` varchar(50) NOT NULL,
+  `EmpCode` varchar(50) DEFAULT NULL,
+  `EmpDept` varchar(120) DEFAULT NULL,
+  `EmpDesignation` varchar(120) DEFAULT NULL,
+  `EmpContactNo` bigint(10) DEFAULT NULL,
+  `EmpGender` enum('Male','Female') DEFAULT NULL,
+  `EmpEmail` varchar(200) DEFAULT NULL,
+  `EmpPassword` varchar(100) DEFAULT NULL,
+  `EmpJoingdate` date DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employeedetail`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(11) NOT NULL,
+  `AdminName` varchar(50) DEFAULT NULL,
+  `AdminuserName` varchar(50) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `AdminuserName`, `Password`, `AdminRegdate`) VALUES
+(1, 'ADMIN', 'admin', 'admin', '2019-10-07 16:52:45');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `empeducation`
+--
+ALTER TABLE `empeducation`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `empexpireince`
+--
+ALTER TABLE `empexpireince`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `employeedetail`
+--
+ALTER TABLE `employeedetail`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `EmpCode` (`EmpCode`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `empeducation`
+--
+ALTER TABLE `empeducation`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `empexpireince`
+--
+ALTER TABLE `empexpireince`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `employeedetail`
+--
+ALTER TABLE `employeedetail`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
